ae7896b0748df43f4cae10aa23eb1fc8c2a66b01
austinbillings <austin@austinbillings.com>
2019-05-28 01:46:41 -0400

    many site additions, incl color palette, typography sass, buttons, fonts, add arkade

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import 'mansion-ui';
+import 'theme/theme.scss';
+
+import MarkupSample from './markup-sample';
+
+ReactDOM.render(<MarkupSample/>, document.getElementById('app-root'));
